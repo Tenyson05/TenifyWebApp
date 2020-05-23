f325f89ae2cbe1cffea726068adb8181c378c0c2
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-05-22 18:55:02 -0700

    added main page styling

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './Login';
-import Tracks from './component/Tracks/Tracks'
+import Tracks from './component/Content/Tracks';
+import Recentlyplayed from './component/Content/RecentlyPlayed';
+import TopArtist from './component/Content/TopArtist';
+import SideMenu from './component/Content/SideMenu';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
+	// <div className="home-page">
+	// 	<SideMenu></SideMenu>,
+	// </div>,
+	
 	<Router>
 		<Route exact path="/" component={Login} />
 		<Route path="/tracks" component={Tracks} />
+		<Route path="/recents" component={Recentlyplayed}/>
+		<Route path="/top" component={TopArtist}/>
 	</Router>,
   document.getElementById('root')
 );
